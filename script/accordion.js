08ab931c957e9ee22e84064a02992ab60be378bd
Smovg-by <74701840+Smovg-by@users.noreply.github.com>
2021-05-18 22:20:49 +0300

    feat: add accordion menu

--- script/accordion.js
+++ script/accordion.js
@@ -1,39 +1,39 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const featureLinkElems = document.querySelectorAll('.feature__link')
+    const featureLinkElems = document.querySelectorAll('.feature__link')
 
-  const featureSubElems = document.querySelectorAll('.feature-sub')
+    const featureSubElems = document.querySelectorAll('.feature-sub')
 
-  //iterate over array var 1
+    //iterate over array var 1
 
-  //   for (let i = 0; i < featureLinkElems.length; i++) {
-  //     featureLinkElems[i].addEventListener('click', () => {
-  //       featureSubElems[i].classList.toggle('hidden')
-  //       featureLinkElems[i].classList.toggle('feature__link_active')
-  //     })
-  //   }
+    //   for (let i = 0; i < featureLinkElems.length; i++) {
+    //     featureLinkElems[i].addEventListener('click', () => {
+    //       featureSubElems[i].classList.toggle('hidden')
+    //       featureLinkElems[i].classList.toggle('feature__link_active')
+    //     })
+    //   }
 
-  //iterate over array var 2
+    //iterate over array var 2
 
-  featureLinkElems.forEach((elem, index) => {
-    elem.addEventListener('click', () => {
-      if (
-        !featureSubElems[index].classList.contains('hidden') &&
-        elem.classList.contains('feature__link_active')
-      ) {
-        featureSubElems[index].classList.add('hidden')
-        elem.classList.remove('feature__link_active')
-      } else {
-        featureSubElems.forEach(elem => {
-          elem.classList.add('hidden') //hide all elements, but one
-        })
+    featureLinkElems.forEach((elem, index) => {
+      elem.addEventListener('click', () => {
+        if (
+          !featureSubElems[index].classList.contains('hidden') &&
+          elem.classList.contains('feature__link_active')
+        ) {
+          featureSubElems[index].classList.add('hidden')
+          elem.classList.remove('feature__link_active')
+        } else {
+          featureSubElems.forEach(elem => {
+            elem.classList.add('hidden') //hide all elements, but one
+          })
 
-        featureLinkElems.forEach(elem => {
-          elem.classList.remove('feature__link_active') //show only one element
-        })
+          featureLinkElems.forEach(elem => {
+            elem.classList.remove('feature__link_active') //show only one element
+          })
 
-        featureSubElems[index].classList.remove('hidden')
-        elem.classList.add('feature__link_active')
-      }
+          featureSubElems[index].classList.remove('hidden')
+          elem.classList.add('feature__link_active')
+        }
+      })
     })
   })
-})
